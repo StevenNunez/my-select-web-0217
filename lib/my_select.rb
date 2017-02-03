9c961e9c92d2c1cda91f8cf9d9563b876db88cf6
Steven Nunez <steven.nunez@gmail.com>
2017-02-03 11:03:23 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  selected = []
+  my_each(collection) do |item|
+    if yield(item)
+      selected << item
+    end
+  end
+  selected
+end
+
+def my_each(items) # put argument(s) here
+  length = items.size
+  i = 0
+  while i < length
+    yield items[i]
+    i += 1
+  end
+  items
 end
